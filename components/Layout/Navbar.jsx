2eeaaca2f36834159b8973ef248b21f6ad40e229
Sabatoor <paul@zimeras.ca>
2023-05-12 20:51:01 -0700

    added footer, added logo image in public, added logo itself to Navbar.js

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,14 +1,23 @@
 // import { PrismicLink } from '@prismicio/react'
 // import Link from 'next/link'
 import Headroom from 'react-headroom'
+import Image from "next/image";
+import * as React from "react";
 // import PrismicButtonLink from '../PrismicButtonLink'
 // import { components } from '../../slices'
 // import { SliceZone } from '@prismicio/react'
 
 export const Navbar = () => {
-  return (
-    <Headroom>
-      <h1>HEADROOM</h1>
-    </Headroom>
-  )
+    return (
+        <header className={`bg-white`}>
+            <Image className={``}
+                   src="/epg-header-logo.svg"
+                   alt="EPG Logo"
+                   width={225}
+                   height={100}/>
+            <Headroom>
+                <h1>HEADROOM</h1>
+            </Headroom>
+        </header>
+    )
 }
