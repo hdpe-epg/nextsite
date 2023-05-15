01c7ffc26d6afc2a6f94122e0727f71550317b4b
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-05-15 18:58:48 -0400

    added base heading

--- slices/Carousell/index.js
+++ slices/Carousell/index.js
@@ -1,88 +1,90 @@
-import {PrismicNextLink} from "@prismicio/next";
-import {PrismicRichText} from "@prismicio/react";
-import Heading from "@/components/Heading";
-import {PrismicNextImage} from "@prismicio/next";
+import { PrismicNextLink } from '@prismicio/next'
+import { PrismicRichText } from '@prismicio/react'
+import Heading from '@/components/Heading'
+import { PrismicNextImage } from '@prismicio/next'
 
 /**
  * @typedef {import("@prismicio/client").Content.CarousellSlice} CarousellSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<CarousellSlice>} CarousellProps
  * @param {CarousellProps} props
  */
-const Carousell = ({slice}) => {
-    // ------------------------------
-    // BEGIN VIDEO CAROUSEL SLICE
-    // ------------------------------
-    if (slice.variation === `videoCarousel`) {
-        return (
-            <section className={`mx-auto my-24 max-w-screen-xl`}>
-                {slice.primary.content && (
-                    <div className={`mb-4`}>
-                        <PrismicRichText
-                            field={slice.primary.content}
-                            components={{
-                                heading2: ({children}) => (
-                                    <Heading as="h2" size="4xl" className="text-center">
-                                        {children}
-                                    </Heading>
-                                ),
-                                paragraph: ({children}) => (
-                                    <p className={`text-center`}>{children}</p>
-                                ),
-                            }}
-                        />
-                    </div>
-                )}
-                {/*grid grid-container--fit*/}
-                <div className="vidCarousel">
-                    {/*This PrismicRichText Field is limited to embed only*/}
-                    {slice.items.map((item, index) => {
-                        console.log("Item:", item);
-                        return (
-                            <div key={index} className={`vidCarousel__inner`}>
-                                <PrismicRichText field={item.youtube_vid_link} />
-                                <Heading as="h2" className={`!text-base`}>{item.youtube_vid_link[0]?.oembed?.title}</Heading>
-                            </div>
-                        );
-                    })}
-                </div>
-            </section>
-        );
-    }
-
-    // ------------------------------
-    // BEGIN DEFAULT CAROUSEL SLICE
-    // ------------------------------
+const Carousell = ({ slice }) => {
+  // ------------------------------
+  // BEGIN VIDEO CAROUSEL SLICE
+  // ------------------------------
+  if (slice.variation === `videoCarousel`) {
     return (
-        <section className={`mx-auto my-24 max-w-screen-xl`}>
-            {slice.primary.content && (
-                <div className={`mb-4`}>
-                    <PrismicRichText
-                        field={slice.primary.content}
-                        components={{
-                            heading2: ({children}) => (
-                                <Heading as="h2" size="4xl" className="text-center">
-                                    {children}
-                                </Heading>
-                            ),
-                            paragraph: ({children}) => (
-                                <p className={`text-center`}>{children}</p>
-                            ),
-                        }}
-                    />
-                </div>
-            )}
-            <div className="grid grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
-                {slice.items.map((item, index) => (
-                    <PrismicNextImage
-                        key={index}
-                        field={item.images}
-                        width={162}
-                        height={75}
-                    />
-                ))}
-            </div>
-        </section>
-    );
-};
+      <section className={`mx-auto my-24 max-w-screen-xl`}>
+        {slice.primary.content && (
+          <div className={`mb-4`}>
+            <PrismicRichText
+              field={slice.primary.content}
+              components={{
+                heading2: ({ children }) => (
+                  <Heading as="h2" size="4xl" className="text-center">
+                    {children}
+                  </Heading>
+                ),
+                paragraph: ({ children }) => (
+                  <p className={`text-center`}>{children}</p>
+                ),
+              }}
+            />
+          </div>
+        )}
+        {/*grid grid-container--fit*/}
+        <div className="vidCarousel">
+          {/*This PrismicRichText Field is limited to embed only*/}
+          {slice.items.map(({ youtube_vid_link }, index) => {
+            const [{ oembed }] = youtube_vid_link
+            return (
+              <div key={index} className={`vidCarousel__inner`}>
+                <PrismicRichText field={youtube_vid_link} />
+                <Heading as="h2" size="base">
+                  {oembed?.title}
+                </Heading>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    )
+  }
 
-export default Carousell;
+  // ------------------------------
+  // BEGIN DEFAULT CAROUSEL SLICE
+  // ------------------------------
+  return (
+    <section className={`mx-auto my-24 max-w-screen-xl`}>
+      {slice.primary.content && (
+        <div className={`mb-4`}>
+          <PrismicRichText
+            field={slice.primary.content}
+            components={{
+              heading2: ({ children }) => (
+                <Heading as="h2" size="4xl" className="text-center">
+                  {children}
+                </Heading>
+              ),
+              paragraph: ({ children }) => (
+                <p className={`text-center`}>{children}</p>
+              ),
+            }}
+          />
+        </div>
+      )}
+      <div className="grid grid-cols-1 justify-center gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8">
+        {slice.items.map((item, index) => (
+          <PrismicNextImage
+            key={index}
+            field={item.images}
+            width={162}
+            height={75}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Carousell
