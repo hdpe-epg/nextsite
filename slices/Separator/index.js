fc3f1beb3b5c40de0f8b48973b66dfb93b7e69e4
Sabatoor <paul@zimeras.ca>
2023-05-16 14:05:58 -0700

    moved separator slice to global.css

--- slices/Separator/index.js
+++ slices/Separator/index.js
@@ -12,11 +12,11 @@ export default Seperator;
 function DefaultSeperatorRender(slice) {
   return (
     <>
-      {slice.primary.need_a_dividor ? (
+      {slice.primary.need_a_dividor && (
         <div className={`my-8`}>
-          <hr className="bg-size-1/2 mx-auto h-px max-w-screen-xl border-none bg-gradient-to-r from-transparent via-gray-500 to-transparent bg-center bg-no-repeat bg-repeat-x" />
+          <hr className="seperator" />
         </div>
-      ) : null}
+      )}
     </>
-  );
+  )
 }
