35d48a04cddcd071b671995b440e14b43a231083
Neil Mastroianni <nf_mastroianni@hotmail.com>
2023-06-08 17:27:08 -0400

    added Inter font and fixed menu close

--- components/Heading.js
+++ components/Heading.js
@@ -2,11 +2,6 @@ import * as React from 'react'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 const Heading = ({ as: Comp = 'h2', size = null, children, className }) => {
-  return (
-    <Comp
-      className={`${className}`}>
-      {children}
-    </Comp>
-  )
+  return <Comp className={`${className} ${inter.className}`}>{children}</Comp>
 }
 export default Heading
